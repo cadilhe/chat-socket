2681acc12b3ae10c27dea2f2e68ce56ba22d36b6
cadilhe <carlos.cadilhe@gmail.com>
2019-11-07 14:44:57 -0200

    mensagens do protocolo

--- src/chat/Comandos.java
+++ src/chat/Comandos.java
@@ -3,7 +3,7 @@ public class Comandos {
 
 	public static final String LISTA_USUARIOS = "lista_usuarios";
 	public static final String SAIR = "sair";
-	public static final String MENSAGEM = "msg:";
+	public static final String MENSAGEM = "mensagem:";
 	public static final String LOGIN = "login:";
 	public static final String LOGIN_ACEITO = "TRUE. O nome de usuário foi registrado com sucesso";
 	public static final String LOGIN_NEGADO = "FALSE. O nome do usuário não pôde ser registrado pois é inválido ou já está em uso";
